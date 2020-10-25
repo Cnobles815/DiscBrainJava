2fee6c8f02bac973468386c028e6c82bcaab48ff
Cnobles815 <Cnobles815@gmail.com>
2020-10-25 14:19:05 -0400

    Attempting cleanup of token issue.

--- src/main/java/ExampleBot.java
+++ src/main/java/ExampleBot.java
@@ -1,13 +1,30 @@
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.channel.MessageChannel;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public final class ExampleBot {
 
-    public static void main(final String[] args) {
-        final String token = "Bob says you no get token.";
+
+
+    public static void main(final String[] args) throws FileNotFoundException {
+        Properties quickProp = new Properties();
+        FileInputStream propRead = new FileInputStream("E:\\Work\\DiscBrainJava\\config.properties");
+
+        try {
+            quickProp.load(propRead);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        final String token = quickProp.getProperty("Token") ;
         final DiscordClient client = DiscordClient.create(token);
         final GatewayDiscordClient gateway = client.login().block();
 
